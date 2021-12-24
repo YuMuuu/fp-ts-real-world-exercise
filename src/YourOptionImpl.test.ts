60ebfc498d331dd0cff462599a84f7e3f4c7a509
yu miura <curelemonade15@gmail.com>
2021-12-25 05:37:59 +0900

    all pass test

--- src/YourOptionImpl.test.ts
+++ src/YourOptionImpl.test.ts
@@ -21,100 +21,100 @@ describe("optPlus", (): void => {
   });
 });
 
-// describe("optPlusGtTen", (): void => {
-//   test("両方Someなら足す, 10以上ならSome", (): void => {
-//     const ret = impl.optPlusGtTen(some(1), some(9));
-//     expect(ret).toStrictEqual(some(10));
-//   });
+describe("optPlusGtTen", (): void => {
+  test("両方Someなら足す, 10以上ならSome", (): void => {
+    const ret = impl.optPlusGtTen(some(1), some(9));
+    expect(ret).toStrictEqual(some(10));
+  });
 
-//   test("両方Someなら足す, 10未満ならNone", (): void => {
-//     const ret = impl.optPlusGtTen(some(1), some(8));
-//     expect(ret).toStrictEqual(none);
-//   });
+  test("両方Someなら足す, 10未満ならNone", (): void => {
+    const ret = impl.optPlusGtTen(some(1), some(8));
+    expect(ret).toStrictEqual(none);
+  });
 
-//   test("NoneがあったらNone(1)", (): void => {
-//     const ret = impl.optPlusGtTen(none, some(8));
-//     expect(ret).toStrictEqual(none);
-//   });
+  test("NoneがあったらNone(1)", (): void => {
+    const ret = impl.optPlusGtTen(none, some(8));
+    expect(ret).toStrictEqual(none);
+  });
 
-//   test("NoneがあったらNone(2)", (): void => {
-//     const ret = impl.optPlusGtTen(some(1), none);
-//     expect(ret).toStrictEqual(none);
-//   });
-// });
+  test("NoneがあったらNone(2)", (): void => {
+    const ret = impl.optPlusGtTen(some(1), none);
+    expect(ret).toStrictEqual(none);
+  });
+});
 
-// describe("optPlusGeTenAsTwicestring", (): void => {
-//   test("両方Someなら足す, 10以上なら2倍String", (): void => {
-//     const ret = impl.optPlusGeTenAsTwicestring(some(1), some(9));
-//     expect(ret).toStrictEqual("20");
-//   });
+describe("optPlusGeTenAsTwicestring", (): void => {
+  test("両方Someなら足す, 10以上なら2倍String", (): void => {
+    const ret = impl.optPlusGeTenAsTwicestring(some(1), some(9));
+    expect(ret).toStrictEqual(some("20"));
+  });
 
-//   test("両方Someなら足す, 10未満ならそのままString", (): void => {
-//     const ret = impl.optPlusGeTenAsTwicestring(some(1), some(8));
-//     expect(ret).toStrictEqual("9");
-//   });
+  test("両方Someなら足す, 10未満ならそのままString", (): void => {
+    const ret = impl.optPlusGeTenAsTwicestring(some(1), some(8));
+    expect(ret).toStrictEqual(some("9"));
+  });
 
-//   test("NoneがあったらNone(1)", (): void => {
-//     const ret = impl.optPlusGeTenAsTwicestring(none, some(8));
-//     expect(ret).toStrictEqual(none);
-//   });
+  test("NoneがあったらNone(1)", (): void => {
+    const ret = impl.optPlusGeTenAsTwicestring(none, some(8));
+    expect(ret).toStrictEqual(none);
+  });
 
-//   test("NoneがあったらNone(2)", (): void => {
-//     const ret = impl.optPlusGeTenAsTwicestring(some(1), none);
-//     expect(ret).toStrictEqual(none);
-//   });
-// });
+  test("NoneがあったらNone(2)", (): void => {
+    const ret = impl.optPlusGeTenAsTwicestring(some(1), none);
+    expect(ret).toStrictEqual(none);
+  });
+});
 
-// describe("optDiv", (): void => {
-//   test("両方Someなら割る", (): void => {
-//     const ret = impl.optDiv(some(10), some(2));
-//     expect(ret).toStrictEqual(some(5));
-//   });
+describe("optDiv", (): void => {
+  test("両方Someなら割る", (): void => {
+    const ret = impl.optDiv(some(10), some(2));
+    expect(ret).toStrictEqual(some(5));
+  });
 
-//   test("両方Someなら割る", (): void => {
-//     const ret = impl.optDiv(some(0), some(10));
-//     expect(ret).toStrictEqual(some(0));
-//   });
+  test("両方Someなら割る", (): void => {
+    const ret = impl.optDiv(some(0), some(10));
+    expect(ret).toStrictEqual(some(0));
+  });
 
-//   test("両方Someなら割る, 0 div はNone", (): void => {
-//     const ret = impl.optDiv(some(10), some(0));
-//     expect(ret).toStrictEqual(none);
-//   });
+  test("両方Someなら割る, 0 div はNone", (): void => {
+    const ret = impl.optDiv(some(10), some(0));
+    expect(ret).toStrictEqual(none);
+  });
 
-//   test("NoneがあったらNone(1)", (): void => {
-//     const ret = impl.optDiv(none, some(2));
-//     expect(ret).toStrictEqual(none);
-//   });
+  test("NoneがあったらNone(1)", (): void => {
+    const ret = impl.optDiv(none, some(2));
+    expect(ret).toStrictEqual(none);
+  });
 
-//   test("NoneがあったらNone(2)", (): void => {
-//     const ret = impl.optDiv(some(1), none);
-//     expect(ret).toStrictEqual(none);
-//   });
-// });
+  test("NoneがあったらNone(2)", (): void => {
+    const ret = impl.optDiv(some(1), none);
+    expect(ret).toStrictEqual(none);
+  });
+});
 
 
-// describe("optFunc", (): void => {
-//   test("両方Someなら関数を当てる（1）", (): void => {
-//     const f = (i: number, j: number) => i + j;
-//     const ret = impl.optFunc(some(10), some(2), f);
-//     expect(ret).toStrictEqual(some(5));
-//   });
+describe("optFunc", (): void => {
+  test("両方Someなら関数を当てる（1）", (): void => {
+    const f = (i: number, j: number) => i + j;
+    const ret = impl.optFunc(some(10), some(2), f);
+    expect(ret).toStrictEqual(some(12));
+  });
 
-//   test("両方Someなら関数を当てる（2）", (): void => {
-//     const f = (i: number, j: number) => i * j;
-//     const ret = impl.optFunc(some(0), some(10), f);
-//     expect(ret).toStrictEqual(some(0));
-//   });
+  test("両方Someなら関数を当てる（2）", (): void => {
+    const f = (i: number, j: number) => i * j;
+    const ret = impl.optFunc(some(0), some(10), f);
+    expect(ret).toStrictEqual(some(0));
+  });
 
-//   test("NoneがあったらNone(1)", (): void => {
-//     const f = (i: number, j: number) => i + j;
-//     const ret = impl.optFunc(none, some(2), f);
-//     expect(ret).toStrictEqual(none);
-//   });
+  test("NoneがあったらNone(1)", (): void => {
+    const f = (i: number, j: number) => i + j;
+    const ret = impl.optFunc(none, some(2), f);
+    expect(ret).toStrictEqual(none);
+  });
 
-//   test("NoneがあったらNone(2)", (): void => {
-//     const f = (i: number, j: number) => i + j;
-//     const ret = impl.optFunc(some(1), none, f);
-//     expect(ret).toStrictEqual(none);
-//   });
-// });
+  test("NoneがあったらNone(2)", (): void => {
+    const f = (i: number, j: number) => i + j;
+    const ret = impl.optFunc(some(1), none, f);
+    expect(ret).toStrictEqual(none);
+  });
+});
--- src/YourOptionImpl.ts
+++ src/YourOptionImpl.ts
@@ -2,6 +2,7 @@ import { Option } from "fp-ts/lib/Option";
 import * as O from "fp-ts/lib/Option";
 import * as F from "fp-ts/lib/function";
 import { OptionOperation } from "./OptionOperation";
+import { number } from "fp-ts";
 
 export class YourOptionImpl implements OptionOperation {
   optPlus(a: Option<number>, b: Option<number>): Option<number> {
@@ -12,22 +13,42 @@ export class YourOptionImpl implements OptionOperation {
     )(a);
   }
   optPlusGtTen(a: Option<number>, b: Option<number>): Option<number> {
-    throw new Error("Method not implemented.");
+    return F.flow(
+      O.bind("_a", () => a),
+      O.bind("_b", () => b),
+      O.map(({ _a, _b }) => _a + _b),
+      O.filter((ab) => ab >= 10)
+    )(a);
   }
   optPlusGeTenAsTwicestring(
     a: Option<number>,
     b: Option<number>
   ): Option<string> {
-    throw new Error("Method not implemented.");
+    return F.flow(
+      O.bind("_a", () => a),
+      O.bind("_b", () => b),
+      O.map(({ _a, _b }) => _a + _b),
+      O.map((ab) => (ab >= 10 ? ab * 2 : ab)),
+      O.map((rs) => String(rs))
+    )(a);
   }
   optDiv(a: Option<number>, b: Option<number>): Option<number> {
-    throw new Error("Method not implemented.");
+    return F.flow(
+      O.bind("_a", () => a),
+      O.bind("_b", () => b),
+      O.filter(({ _b }) => _b !== 0),
+      O.map(({ _a, _b }) => _a / _b)
+    )(a);
   }
   optFunc(
     a: Option<number>,
     b: Option<number>,
     f: (arg0: number, arg1: number) => number
   ): Option<number> {
-    throw new Error("Method not implemented.");
+    return F.flow(
+      O.bind("_a", () => a),
+      O.bind("_b", () => b),
+      O.map(({ _a, _b }) => f(_a, _b))
+    )(a);
   }
 }
